30db5050468392f600d42650445bfac11ae7c0a3
Drew Machat <drew@alleyinteractive.com>
2015-09-24 13:43:13 -0400

    sync from the wiki

---
+++ scripts/sync-wiki.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# go to the content directory and create a *new* Git repo
+cd content
+
+# Remove possible existing git repo. We'll replace entire wiki
+rm -rf .git
+
+# Init new repo
+git init
+
+# Add origin
+git remote add origin git@github.com:dmachat/angular-webpack-cookbook.wiki.git
+
+# Reset any local changes
+git clean -fd
+git reset --hard HEAD
+
+# Update from remote wiki
+git pull origin master:master
+
+# Finally remove the git repository to keep changes versioned in the parent repo
+rm -rf .git
